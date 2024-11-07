00ef34de8fc2e03d61ce206139040d0534bb0f22
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-11-07 03:29:31 +0100

    fix build error

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,24 +1,25 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import EventForm from "@/app/Components/EventForm";
 
-// const Admin = () => {
-//   const [events, setEvents] = useState([]);
+const Admin = () => {
+  const [events, setEvents] = useState([]);
 
-//   useEffect(() => {
-//     axios.get("/api/events").then((response) => setEvents(response.data.data));
-//   }, []);
+  useEffect(() => {
+    axios.get("/api/events").then((response) => setEvents(response.data.data));
+  }, []);
 
-//   return (
-//     <div>
-//       <h1>Manage Events</h1>
-//       <EventForm />
-//       <ul>
-//         {events.map((event) => (
-//           <li key={event._id}>{event.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Manage Events</h1>
+      <EventForm />
+      <ul>
+        {events.map((event) => (
+          <li key={event._id}>{event.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-// export default Admin;
+export default Admin;
